863234512ed03401e715d523ec6392a5deeffca8
Baver Kaçar <baverkacar@gmail.com>
2021-08-13 02:21:43 +0300

    listings and searching is done

--- bkemlak/settings.py
+++ bkemlak/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +137,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
